50a44c52e12ac0678c42fb1929fea80e4eb7a671
ShizamDaGeek <150592972+ShizamDaGeek@users.noreply.github.com>
2024-04-10 10:00:48 +1200

    wfrewnhivrw

--- code-snippet-and-other.js
+++ code-snippet-and-other.js
@@ -1,7 +1,7 @@
-$(document).ready(function() 
+$(document).ready(function () 
 {
-    $('textarea').linenumbers({col_width:'75px', start: 0, gutter_width:'auto',});	
-    $.getJSON("code-snippets.json", data,
-
-    );
-});
+    $.getJSON("code-snippets.json", function (data) 
+    {
+        
+    });
+})
