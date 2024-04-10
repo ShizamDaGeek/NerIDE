caa25a610741d2d5e6e04a299817e5de42e45122
ShizamDaGeek <150592972+ShizamDaGeek@users.noreply.github.com>
2024-04-11 07:54:20 +1200

    almost done with the custom menubar

--- main.js
+++ main.js
@@ -2,20 +2,28 @@ const { app, BrowserWindow } = require('electron');
 const path = require('path');
 const electronReload = require('electron-reload');
 
-function createWindow() {
-    const win = new BrowserWindow({
+function createWindow() 
+{
+    const win = new BrowserWindow
+    ({
         width: 800,
         height: 600,
-        webPreferences: {
+        frame: false,
+        webPreferences: 
+        {
+            nodeIntegration: true,
+            contextIsolation: false,
             preload: path.join(__dirname, 'preload.js'),
         }
     });
 
     win.loadFile('index.html');
     win.maximize(true);
+    win.setMenuBarVisibility(false);
 }
 
-app.whenReady().then(() => {
+app.whenReady().then(() => 
+{
     createWindow();
 
     app.on('activate', () => {
@@ -29,7 +37,8 @@ app.whenReady().then(() => {
     });
 });
 
-app.on('window-all-closed', () => {
+app.on('window-all-closed', () => 
+{
     if (process.platform !== 'darwin') {
         app.quit();
     }
