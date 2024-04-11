d9fb799ab1c0c1e0582aecd500e5363fee0f3d6e
ShizamDaGeek <150592972+ShizamDaGeek@users.noreply.github.com>
2024-04-11 14:37:22 +1200

    IT WORKS im Cumbust

--- main.js
+++ main.js
@@ -11,8 +11,6 @@ function createWindow()
         frame: false,
         webPreferences: 
         {
-            nodeIntegration: true,
-            contextIsolation: false,
             preload: path.join(__dirname, 'preload.js'),
         }
     });
