fa74632bd1aa048370db84003e9040a1bf1e6357
ShizamDaGeek <150592972+ShizamDaGeek@users.noreply.github.com>
2024-05-31 23:01:28 +1200

    Thing's added/removed/changed
    STUFF ADDED: A really basic as vertical number line that broke then scrolling thing so gonna have to fix that
    
    STUFF REMOVED: The tab bar is removed for now until I fix the vertical number line
    
    STUFF CHANGED: Nothing

--- src/content.js
+++ src/content.js
@@ -1,8 +1,38 @@
 $(document).ready(function() 
 {
-    /* TOGGLE TO HIDE OR SHOW FILE EXPLORER*/
+    // TOGGLE TO HIDE OR SHOW FILE EXPLORER
     $('.hide-file').click(function () 
     { 
         $('.file-explorer').toggle();
+    });
+
+    // ADD A VERTICAL NUMBER LINE IN THE TEXTAREA
+    const $textarea = $('textarea');
+    const $lineNumbers = $('.vertical-number-line');
+
+    $textarea.on('keyup', function(event) {
+    const numberOfLines = $(this).val().split('\n').length;
+
+    $lineNumbers.html(new Array(numberOfLines)
+        .fill('<span></span>')
+        .join(''));
+    });
+
+    // TAB SYSTEM FOR INDENTATION
+    $('textarea').on('keydown', function(event) 
+    {
+        if (event.key === 'Tab') 
+        {
+            const textarea = this;
+            const start = textarea.selectionStart;
+            const end = textarea.selectionEnd;
+
+            const tabSpaces = '    ';
+            textarea.value = textarea.value.substring(0, start) + tabSpaces + textarea.value.substring(end);
+            textarea.selectionStart = textarea.selectionEnd = start + tabSpaces.length;
+            textarea.focus();
+
+            event.preventDefault();
+        }
     });
 });
