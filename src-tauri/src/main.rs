7bca8ba340f8e6db24d92f2c0662b181c3c3792b
ShizamDaGeek <150592972+ShizamDaGeek@users.noreply.github.com>
2024-07-11 16:00:20 +1200

    I am making some graphical improvements to the IDE and also making a menu bar

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,51 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use tauri::{CustomMenuItem, Menu, MenuItem, Submenu, Wry};
 
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+  let new = CustomMenuItem::new("new".to_string(), "New");
+  let open = CustomMenuItem::new("open".to_string(), "Open");
+  let exit = CustomMenuItem::new("exit".to_string(), "Exit");
+  let file_menu = Submenu::new("File", Menu::new().add_item(new).add_item(open).add_native_item(MenuItem::Separator).add_item(exit));
+
+  let undo = CustomMenuItem::new("undo".to_string(), "Undo");
+  let redo = CustomMenuItem::new("redo".to_string(), "Redo");
+  let edit_menu = Submenu::new("Edit", Menu::new().add_item(undo).add_item(redo));
+
+  let about = CustomMenuItem::new("about".to_string(), "About");
+  let help_menu = Submenu::new("Help", Menu::new().add_item(about));
+
+  let menu = Menu::new()
+    .add_submenu(file_menu)
+    .add_submenu(edit_menu)
+    .add_submenu(help_menu);
+
+  tauri::Builder::default()
+    .menu(menu)
+    .on_menu_event(|event| match event.menu_item_id() {
+      "new" => {
+        println!("New was clicked");
+      }
+      "open" => {
+        println!("Open was clicked");
+      }
+      "exit" => {
+        std::process::exit(0);
+      }
+      "undo" => {
+        println!("Undo was clicked");
+      }
+      "redo" => {
+        println!("Redo was clicked");
+      }
+      "about" => {
+        println!("About was clicked");
+      }
+      _ => {}
+    })
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
